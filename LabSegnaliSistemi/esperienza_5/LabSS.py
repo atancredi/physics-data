cbe12daf2fe42659d491f1e46c8ec07b17a7541c
Alessandro Tancredi <83644336+atancredi@users.noreply.github.com>
2021-11-15 14:51:45 +0100

    aggiornamento

---
+++ LabSegnaliSistemi/esperienza_5/LabSS.py
@@ -0,0 +1,5 @@
+def hint_nu():
+	# Frequenze suggerite:
+	print(np.logspace(1,2.8, 4, base=10).round(-1))
+	print(np.logspace(2.8,5.5, 20,base=10).round(-1)[1:])
+	print(np.logspace(5.5, 6, 3,).round(-4)[1:])
