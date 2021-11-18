ec39c8149235fda7b40808948ec49b7ea696619e
Alessandro Tancredi <83644336+atancredi@users.noreply.github.com>
2021-11-18 16:04:46 +0100

    .

--- PhysicsFramework/PhysicsFramework/LabSS.py
+++ PhysicsFramework/PhysicsFramework/LabSS.py
@@ -15,6 +15,13 @@ mpl.rcParams['figure.dpi'] = 100
 #from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 #import matplotlib.ticker as mticker
 
+##########################################################
+#Configuration
+
+oscilloscope_error = 0.03 #uncertainty when reading the oscilloscope
+
+##########################################################
+#Core program
 
 #Suggested frequencies: evenly spaced values of the frequency on the log scale
 #useful when measuring the bode diagram of a circuit
@@ -26,28 +33,70 @@ def hint_nu(join):
 
     return np.sort(np.hstack((np.logspace(1,2.8, 4, base=10).round(-1),np.logspace(2.8,5.5, 20,base=10).round(-1)[1:],join)).astype(int)).tolist()
 
-#Calculate values for plotting the bode diagram
-def BodeCalculations(vout, vin, freq, deltat):
+#Create the dataset from the direct data
+def dataSet(freq, vin, vout, deltat):
+    return {
+        "freq": [freq,None],
+        "vin": [vin,None],
+        "vout": [vout,None],
+        "module": [None,None],
+        "deltat": [deltat,None],
+        "deltaphi": [None, None]
+        }
 
-    #calculate the module of the transfer function
-    module = vout[0]/vin[0]
-    freq_act = freq[0]*10**3
-    deltat_act = deltat[0]*(10**-6)  #us
-    delta_phi = ((2*np.pi*freq_act*deltat_act)*(180/np.pi))
-    sigma_freq = np.around((freq[0]*10**3)*0.03, 3)
-    rel_freq = sigma_freq/freq[0]
-    sigma_vin = np.around(vin[0]*0.03,2)
-    rel_vin = sigma_vin/vin[0]
-    sigma_vout = np.around(vout[0]*0.03,2)
-    rel_vout = sigma_vout/vout[0]
-    sigma_deltat = np.around(deltat[0]*0.03,3)
-    rel_deltat = sigma_deltat/deltat[0]
-    
+
+#Unpack the dataSet to get the columns
+def unpack_dataSet(dataSet, type):
+    if type == "values":
+        return dataSet["freq"][0], dataSet["vin"][0], dataSet["vout"][0], dataSet["deltat"][0]
+    elif type == "errors":
+        return dataSet["freq"][1], dataSet["vin"][1], dataSet["vout"][1], dataSet["deltat"][1]
+    else:
+        return 0
+
+#Calculate values for plotting the bode diagram
+def BodeCalculations(dataSet):
+
+    #get the data from the dataSet
+    freq, vin, vout, deltat = unpack_dataSet(dataSet, "values")
+
+    #scalate the data (measurement-unit-wise)
+    freq = freq*10**3
+    deltat = deltat[0]*(10**-6)  #us
+
+    #calculate the uncertainty on the acquired data based on the specs from the oscilloscope
+    sigma_freq = freq*oscilloscope_error
+    rel_freq = sigma_freq/freq
+
+    sigma_vin = vin*oscilloscope_error
+    rel_vin = sigma_vin/vin
+
+    sigma_vout = vout*oscilloscope_error
+    rel_vout = sigma_vout/vout
+
+    sigma_deltat = deltat*oscilloscope_error
+    rel_deltat = sigma_deltat/deltat
+
+    #calculate the module of the transfer function, with its error
+    module = vout/vin
     sigma_module = rel_vin + rel_vout
+
+    #calculate the phase difference between vin and vout, with its error
+    delta_phi = ((2*np.pi*freq*deltat)*(180/np.pi))
     sigma_deltaphi = (rel_deltat + rel_freq)
 
-#Plot the bode diagram from a raw dataset
-def Bode(vout, vin, freq, deltat, mode):
+    #put the data inside the dataSet and return it
+    dataSet["freq"][1] = sigma_freq
+    dataSet["vin"][1] = sigma_vin
+    dataSet["vout"][1] = sigma_vout
+    dataSet["deltat"][1] = sigma_deltat
+    dataSet["module"][0] = module
+    dataSet["module"][1] = sigma_module
+    dataSet["deltaphi"][0] = delta_phi
+    dataSet["deltaphi"][1] = sigma_deltaphi
+
+#Plot the bode diagram from a dataset
+def Bode(dataSet):
     
     
     if(mode != "nograph"):
@@ -76,7 +125,7 @@ def Bode(vout, vin, freq, deltat, mode):
     return module, sigma_module, delta_phi, sigma_deltaphi
 
 
-##########################################################à
+##########################################################
 #Useful functions for plotting with matplotlib
 def CreateFigure():
     return len(plt.get_fignums())
